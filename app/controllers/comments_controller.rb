7600ace66b71504691e5b55dec661e8febc89b4c
fumidesu <fumilein23@yahoo.co.jp>
2017-06-25 15:26:44 +0000

    edit

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,7 +7,8 @@ class CommentsController < ApplicationController
     # クライアント要求に応じてフォーマットを変更
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to blog_path(@blog), notice: 'コメントを投稿しました。' }
+        format.html { redirect_to blog_path(@blog)}
+          flash[:notice] = "コメントを投稿しました。"
         # JS形式でレスポンスを返します。
         format.js { render :index }
       else
@@ -21,12 +22,40 @@ class CommentsController < ApplicationController
     @comment = Comment.find(params[:id])
     @comment.destroy
     respond_to do |format|
-        format.html {redirect_to blogs_path(@blog), flash[:notice] = 'コメントを削除しました！'}
+        format.html {redirect_to blogs_path(@blog)}
+          flash[:notice] = "コメントを削除しました！"
         format.js { render :index }
     end
   end
 #追加終了
+def edit
+end
 
+#編集追加
+def update
+  @comment = Comment.find(params[:id])
+  if @comment.update
+     redirect_to blogs_path(@blog), notice: "コメントを編集しました！"
+  else
+    render 'edit'
+  end
+end
+
+
+#def update
+  #  @comment = Comment.find(params[:id])
+
+  #  respond_to do |format|
+  #    if @comment.update(comment_params)
+  #      format.html {redirect_to blogs_path(@blog)}
+  #      flash[:notice] = "ブログを編集しました！"
+  #      format.js { render :index }
+  #    else
+  #      format.html { render :edit}
+  #    end
+  #  end
+#end
+#編集終了
 
   private
     # ストロングパラメーター
